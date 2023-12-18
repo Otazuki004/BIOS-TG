54d316304ed01bfd2d61279eaeee7f4ba1bcfd53
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-19 00:10:08 +0530

    Update Boot.py

--- Boot.py
+++ Boot.py
@@ -1,6 +1,8 @@
 from vars import *
 from pyrogram import *
 
-os = None
-print ("setup process starting...")
-try:
+os = None #Hou much os you have
+if os < 0:
+    print ("setup process starting...")
+elif os 
+
