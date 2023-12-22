e5ae4b889f919a0edec8a2c78c451e5de2e9a9ef
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-22 08:48:16 +0530

    UPDATED

--- Boot.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import *
-
-os = 1 #Hou much os you have
-if os < 0:
-    print ("setup process starting...")
-elif os == 1:
-    print("Booting")
-elif os > 1:
-    print("What os you want boot")
